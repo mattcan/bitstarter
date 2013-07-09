29d0760c6c23dd447e4ae91a4837362cec234b94
Matthew Cantelon <me@matthewcantelon.ca>
2013-07-09 03:11:18 +0000

    Read text from file - fix

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  var b = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+  var b = fs.readFileSync('index.html');
 
   response.send(b.toString);
 });
