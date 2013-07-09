e87b38c3bc11ac856cb8aaebe0e481e0146318a5
Matthew Cantelon <me@matthewcantelon.ca>
2013-07-09 03:09:18 +0000

    Read text from file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var b = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+
+  response.send(b.toString);
 });
 
 var port = process.env.PORT || 5000;
