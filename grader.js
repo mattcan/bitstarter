1599e91c669b2634046e48ee2268fa0f2b0d6239
Matthew Cantelon <me@matthewcantelon.ca>
2013-07-10 05:58:06 +0000

    Updated grader to check url and save results

--- grader.js
+++ grader.js
@@ -4,8 +4,10 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://peaceful-bastion-8967.herokuapp.com"
 
 var assertFileExists = function(infile) {
   var instr = infile.toString();
@@ -42,21 +44,35 @@ var clone = function(fn) {
   return fn.bind({});
 };
 
-var rGet = function(fn) {
-  return (rest.get(fn).on('complete', function(result) {
-    return result.toString();
-  }));
+var checkUrl = function(url) {
+  rest.get(url).on('complete', function(result) {
+    if(result instanceof Error) {
+      return 'Error: ' + result.message;
+      this.retry(5000);
+    }
+    else {
+      return result;
+    }
+  });
 };
 
 if(require.main == module) {
   program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-    .option('-u, --url <url>', 'URL to index.html', rGet(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'URL to index.html', clone(checkUrl), URL_DEFAULT)
     .parse(process.argv);
 
   var checkJson = checkHtmlFile(program.file, program.checks);
   var outJson = JSON.stringify(checkJson, null, 4);
+
+  fs.writeFile("results.json", outJson, function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("The file was saved");
+    }
+  });
   console.log(outJson);
 } else {
   exports.checkHtmlFile = checkHtmlFile;
